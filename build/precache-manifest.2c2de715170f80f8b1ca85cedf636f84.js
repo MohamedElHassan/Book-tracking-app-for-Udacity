495d2bd5cc4c647273d9e551e0feda0479abc985
Mohammed El Hassan <mohammedelhassan2001@gmail.com>
2021-09-09 00:26:19 +0200

    Added the Production File

---
+++ build/precache-manifest.2c2de715170f80f8b1ca85cedf636f84.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "3e9dd033da5ded8f4d42",
+    "url": "/static/css/main.39c32a5a.chunk.css"
+  },
+  {
+    "revision": "3e9dd033da5ded8f4d42",
+    "url": "/static/js/main.3e9dd033.chunk.js"
+  },
+  {
+    "revision": "c400c9cfccb90f2acab7",
+    "url": "/static/js/1.c400c9cf.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "75d390e5eabc3254418e950df631389b",
+    "url": "/static/media/add.75d390e5.svg"
+  },
+  {
+    "revision": "9d4e3f366f039249695de85a30a02b95",
+    "url": "/static/media/arrow-drop-down.9d4e3f36.svg"
+  },
+  {
+    "revision": "20e8847d1eb7759c19b0e30beb8849cf",
+    "url": "/static/media/arrow-back.20e8847d.svg"
+  },
+  {
+    "revision": "70be31743671a8ab11893be6a74b7718",
+    "url": "/index.html"
+  }
+];
